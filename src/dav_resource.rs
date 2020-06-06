d2223122961ca2d148842dfe4c49879eb618e402
Julian Hofer <julian.git@mailbox.org>
2020-06-06 22:11:22 +0200

    Enable basic tests

--- src/dav_resource.rs
+++ src/dav_resource.rs
@@ -1,4 +1,39 @@
 use isahc::HttpClient;
-pub(crate) struct DavResource {
-    client: HttpClient,
+pub struct DavResource {
+    pub client: HttpClient,
+}
+
+impl DavResource {
+    pub fn new(client: HttpClient) -> Self {
+        Self { client }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DavResource;
+    use isahc::prelude::*;
+    use mockito::mock;
+
+    #[test]
+    fn test_something() {
+        let base_url = "/remote.php/dav";
+        let m = mock("DELETE", base_url)
+            .with_status(201)
+            .with_body("test")
+            .create();
+
+        let http_client = HttpClient::new().unwrap();
+        let dav_resource = DavResource::new(http_client);
+        let uri = format!("{}{}", mockito::server_url(), base_url);
+        let request = Request::builder()
+            .method("DELETE")
+            .uri(uri)
+            .body(())
+            .unwrap();
+        let mut response = dav_resource.client.send(request).unwrap();
+        m.assert();
+        assert_eq!(response.text().unwrap(), "test");
+        assert_eq!(response.status(), 201);
+    }
 }
