c88e66b80bfd583957fbb1c97590a39702795e9a
Julian Hofer <julian.git@mailbox.org>
2020-06-10 19:07:54 +0200

    Start with basic authentification

--- src/dav_resource.rs
+++ src/dav_resource.rs
@@ -1,4 +1,11 @@
+use base64;
+use base64::write::EncoderWriter as Base64Encoder;
+use isahc::http::header;
+use isahc::prelude::*;
 use isahc::HttpClient;
+use std::fmt;
+use std::io::Write;
+
 pub struct DavResource {
     pub client: HttpClient,
 }
@@ -9,10 +16,36 @@ impl DavResource {
     }
 }
 
+fn basic_auth<T, U, P>(
+    mut request: Request<T>,
+    response: &Response<T>,
+    username: U,
+    password: Option<P>,
+) -> Request<T>
+where
+    U: fmt::Display,
+    P: fmt::Display,
+{
+    let mut header_value = b"Basic ".to_vec();
+    {
+        let mut encoder = Base64Encoder::new(&mut header_value, base64::STANDARD);
+        // The unwraps here are fine because Vec::write* is infallible.
+        write!(encoder, "{}:", username).unwrap();
+        if let Some(password) = password {
+            write!(encoder, "{}", password).unwrap();
+        }
+    }
+
+    request.headers_mut().insert(
+        header::AUTHORIZATION,
+        isahc::http::HeaderValue::from_bytes(&header_value).unwrap(),
+    );
+    request
+}
+
 #[cfg(test)]
 mod tests {
-    use super::DavResource;
-    use isahc::prelude::*;
+    use super::*;
     use mockito::mock;
 
     #[test]
@@ -35,5 +68,27 @@ mod tests {
         m.assert();
         assert_eq!(response.text().unwrap(), "test");
         assert_eq!(response.status(), 201);
+    }
+
+    #[test]
+    fn test_basic_ascii() {
+        let original = Request::builder()
+            .uri("http://example.com")
+            .body(())
+            .unwrap();
+
+        let response = Response::new(());
+
+        let request = basic_auth(original, &response, "user", Some("password"));
+
+        assert_eq!(
+            request.headers().get(header::AUTHORIZATION).unwrap(),
+            "Basic dXNlcjpwYXNzd29yZA=="
+        );
+    }
+
+    #[test]
+    fn test_basic_utf8() {
+        todo!()
     }
 }
